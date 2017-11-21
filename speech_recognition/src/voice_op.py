d51354728c3fbabe1120c688de34e52374cfdb82
lxb550 <lxb550@cs.bham.ac.uk>
2017-11-21 18:32:25 +0000

    Should control robot movement

--- speech_recognition/src/voice_op.py
+++ speech_recognition/src/voice_op.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python
 
 import rospy
-from geometry_msgs.msg import Twist
 from std_msgs.msg import String
 
-class RobotVoiceTeleop:
+class RobotVoiceOperation:
     #define the constructor of the class
     def  __init__(self):
-        #initialize the ROS node with a name voice_teleop
+        #initialize the ROS node with a name voice_op
         rospy.init_node('voice_op')
 
-        # Publish the Twist message to the cmd_vel topic
-        self.cmd_vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
+        # Publish the String message to the audio_conntrol topic
+        self.movement_pub= rospy.Publisher('audio_control', String, queue_size=5)
 
         # Subscribe to the /recognizer/output topic to receive voice commands.
         rospy.Subscriber('/recognizer/output', String, self.voice_command_callback)
@@ -19,30 +18,27 @@ class RobotVoiceTeleop:
         #create a Rate object to sleep the process at 5 Hz
         rate = rospy.Rate(5)
 
-        # Initialize the Twist message we will publish.
-        self.cmd_vel = Twist()
-        #make sure to make the robot stop by default
-        self.cmd_vel.linear.x=0;
-        self.cmd_vel.angular.z=0;
+        # Initialize the movement command message we will publish.
+        self.movement_str = '0'
 
 
 
         # A mapping from keywords or phrases to commands
         #we consider the following simple commands, which you can extend on your own
-        self.commands =        ['start-stream',
-                                'end-stream',
-                                'go-away',
-                                'come-here',
+        self.commands =        ['forward',
+                                'slight-left',
+                                'slight-right',
                                 'stop',
-                                'forward',
+                                'rotate-left',
+                                'rotate-right',
                                 'backward',
-                                'turn left',
-                                'turn right',
+                                'voice control',
+                                'twitch control',
                                 ]
         rospy.loginfo("Ready to receive voice commands")
-        # We have to keep publishing the cmd_vel message if we want the robot to keep moving.
+        # We have to keep publishing the movement_str message if we want the robot to keep moving.
         while not rospy.is_shutdown():
-            self.cmd_vel_pub.publish(self.cmd_vel)
+            self.movement_pub.publish(self.movement_str)
             rate.sleep()
 
 
@@ -50,36 +46,35 @@ class RobotVoiceTeleop:
         # Get the motion command from the recognized phrase
         command = msg.data
         if (command in self.commands):
-            if command == 'start-stream':
-                #TODO
-            elif command == 'forward':
-                self.cmd_vel.linear.x = 0.2
-                self.cmd_vel.angular.z = 0.0
-            elif command == 'backward':
-                self.cmd_vel.linear.x = -0.2
-                self.cmd_vel.angular.z = 0.0
-            elif command == 'turn left':
-                self.cmd_vel.linear.x = 0.0
-                self.cmd_vel.angular.z = 0.5
-            elif command == 'turn right':
-                self.cmd_vel.linear.x = 0.0
-                self.cmd_vel.angular.z = -0.5
+            if command == 'forward':
+                self.movement_str = '1'
+            elif command == 'slight-left':
+                self.movement_str = '2'
+            elif command == 'slight-right':
+                self.movement_str = '3'
             elif command == 'stop':
-                self.cmd_vel.linear.x = 0.0
-                self.cmd_vel.angular.z = 0.0
+                self.movement_str = '4'
+            elif command == 'rotate-left':
+                self.movement_str = '5'
+            elif command == 'rotate-right':
+                self.movement_str = '6'
+            elif command == 'backward':
+                self.movement_str = '7'
+            elif command == 'voice-control':
+                #TODO
+            elif command == 'twitch-control':
+                #TODO
 
         else: #command not found
-            #print 'command not found: '+command
-            self.cmd_vel.linear.x = 0.0
-            self.cmd_vel.angular.z = 0.0
-        print ("linear speed : " + str(self.cmd_vel.linear.x))
-        print ("angular speed: " + str(self.cmd_vel.angular.z))
+            print 'command not found: '+command
+            self.movement_str = '0'
+        print ("command: " + command)
 
 
 
 if __name__=="__main__":
     try:
-      RobotVoiceTeleop()
+      RobotVoiceOperation()
       rospy.spin()
     except rospy.ROSInterruptException:
       rospy.loginfo("Voice navigation terminated.")
